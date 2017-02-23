66464d190b024e2779f499fc9efffcaead245803
ozzzi <oz@oz-web.com>
2017-02-23 19:00:34 +0100

    + Added main class with info about usage

---
+++ src/app/JWT4B.java
@@ -0,0 +1,8 @@
+package app;
+
+public class JWT4B {
+	public static void main(String[] args) {
+		// This also makes the export not warn about not finding a main method
+		System.out.println("This JAR is meant to be run through BURP extender");
+	}
+}
