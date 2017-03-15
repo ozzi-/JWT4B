12fa03eb4a97cdb7a971efc99cc25d3cbc2d6a99
Oussama Zgheb <oz@oz-web.com>
2017-03-15 13:35:30 +0100

    Added support for ES256,384, 512

--- src/app/algorithm/AlgorithmLinker.java
+++ src/app/algorithm/AlgorithmLinker.java
@@ -4,6 +4,7 @@ import java.io.UnsupportedEncodingException;
 import java.security.KeyFactory;
 import java.security.NoSuchAlgorithmException;
 import java.security.PublicKey;
+import java.security.interfaces.ECKey;
 import java.security.interfaces.RSAKey;
 import java.security.spec.EncodedKeySpec;
 import java.security.spec.InvalidKeySpecException;
@@ -16,29 +17,42 @@ import app.helpers.ConsoleOut;
 
 public class AlgorithmLinker {
 
-	public static final app.algorithm.AlgorithmWrapper none = new app.algorithm.AlgorithmWrapper("none", AlgorithmType.none);
-	public static final app.algorithm.AlgorithmWrapper HS256 = new app.algorithm.AlgorithmWrapper("HS256", AlgorithmType.symmetric);
-	public static final app.algorithm.AlgorithmWrapper HS384 = new app.algorithm.AlgorithmWrapper("HS384", AlgorithmType.symmetric);
-	public static final app.algorithm.AlgorithmWrapper HS512 = new app.algorithm.AlgorithmWrapper("HS512", AlgorithmType.symmetric);
-	public static final app.algorithm.AlgorithmWrapper RS256 = new app.algorithm.AlgorithmWrapper("RS256", AlgorithmType.asymmetric);
-	public static final app.algorithm.AlgorithmWrapper RS384 = new app.algorithm.AlgorithmWrapper("RS384", AlgorithmType.asymmetric);
-	public static final app.algorithm.AlgorithmWrapper RS512 = new app.algorithm.AlgorithmWrapper("RS512", AlgorithmType.asymmetric);
-	public static final app.algorithm.AlgorithmWrapper ES256 = new app.algorithm.AlgorithmWrapper("ES256", AlgorithmType.asymmetric);
-	public static final app.algorithm.AlgorithmWrapper ES384 = new app.algorithm.AlgorithmWrapper("ES384", AlgorithmType.asymmetric);
-	public static final app.algorithm.AlgorithmWrapper ES512 = new app.algorithm.AlgorithmWrapper("ES512", AlgorithmType.asymmetric);
+	public static final app.algorithm.AlgorithmWrapper none = new app.algorithm.AlgorithmWrapper("none",
+			AlgorithmType.none);
+	public static final app.algorithm.AlgorithmWrapper HS256 = new app.algorithm.AlgorithmWrapper("HS256",
+			AlgorithmType.symmetric);
+	public static final app.algorithm.AlgorithmWrapper HS384 = new app.algorithm.AlgorithmWrapper("HS384",
+			AlgorithmType.symmetric);
+	public static final app.algorithm.AlgorithmWrapper HS512 = new app.algorithm.AlgorithmWrapper("HS512",
+			AlgorithmType.symmetric);
+	public static final app.algorithm.AlgorithmWrapper RS256 = new app.algorithm.AlgorithmWrapper("RS256",
+			AlgorithmType.asymmetric);
+	public static final app.algorithm.AlgorithmWrapper RS384 = new app.algorithm.AlgorithmWrapper("RS384",
+			AlgorithmType.asymmetric);
+	public static final app.algorithm.AlgorithmWrapper RS512 = new app.algorithm.AlgorithmWrapper("RS512",
+			AlgorithmType.asymmetric);
+	public static final app.algorithm.AlgorithmWrapper ES256 = new app.algorithm.AlgorithmWrapper("ES256",
+			AlgorithmType.asymmetric);
+	public static final app.algorithm.AlgorithmWrapper ES384 = new app.algorithm.AlgorithmWrapper("ES384",
+			AlgorithmType.asymmetric);
+	public static final app.algorithm.AlgorithmWrapper ES512 = new app.algorithm.AlgorithmWrapper("ES512",
+			AlgorithmType.asymmetric);
 
-	private static final app.algorithm.AlgorithmWrapper[] supportedAlgorithms = {none, HS256, HS384, HS512, RS256, RS384, RS512, ES256, ES384, ES512};
+	private static final app.algorithm.AlgorithmWrapper[] supportedAlgorithms = { 
+			none, HS256, HS384, HS512, RS256, RS384, RS512, ES256, ES384, ES512 };
 
 	private static PublicKey generatePublicKeyFromString(String key, String algorithm) {
 		PublicKey publicKey = null;
-		key = key.replace("-----BEGIN PUBLIC KEY-----", "").replace("-----END PUBLIC KEY-----", "").replaceAll("\\s+", "").replaceAll("\\r+", "").replaceAll("\\n+", "");
+		key = key.replace("-----BEGIN PUBLIC KEY-----", "").replace("-----END PUBLIC KEY-----", "")
+				.replaceAll("\\s+", "").replaceAll("\\r+", "").replaceAll("\\n+", "");
 		byte[] keyByteArray = Base64.getDecoder().decode(key);
 		try {
 			KeyFactory kf = KeyFactory.getInstance(algorithm);
 			EncodedKeySpec keySpec = new X509EncodedKeySpec(keyByteArray);
 			publicKey = kf.generatePublic(keySpec);
 		} catch (NoSuchAlgorithmException e) {
-			ConsoleOut.output("Could not reconstruct the public key, the given algorithm could not be found - " + e.getMessage());
+			ConsoleOut.output(
+					"Could not reconstruct the public key, the given algorithm could not be found - " + e.getMessage());
 		} catch (InvalidKeySpecException e) {
 			ConsoleOut.output("Could not reconstruct the public key - " + e.getMessage());
 		}
@@ -47,12 +61,14 @@ public class AlgorithmLinker {
 
 	/**
 	 * @param algo
-	 * @param key  , either the secret or the private key
-	 * @return the algorithm element from the library, if nothing matches the none algorithm element is returned
+	 * @param key - either the secret or the private key
+	 * @return the algorithm element from the library, if nothing matches the
+	 *         none algorithm element is returned
 	 * @throws IllegalArgumentException
 	 * @throws UnsupportedEncodingException
 	 */
-	public static Algorithm getAlgorithm(String algo, String key) throws IllegalArgumentException, UnsupportedEncodingException {
+	public static Algorithm getAlgorithm(String algo, String key)
+			throws IllegalArgumentException, UnsupportedEncodingException {
 		if (algo.equals(HS256.getAlgorithm())) {
 			return Algorithm.HMAC256(key);
 		}
@@ -63,13 +79,13 @@ public class AlgorithmLinker {
 			return Algorithm.HMAC512(key);
 		}
 		if (algo.equals(ES256.getAlgorithm())) {
-			return Algorithm.HMAC384(key); // TODO implement ES Algos properly
+			return Algorithm.ECDSA256((ECKey) generatePublicKeyFromString(key, "EC"));
 		}
 		if (algo.equals(ES384.getAlgorithm())) {
-			return Algorithm.HMAC384(key);
+			return Algorithm.ECDSA384((ECKey) generatePublicKeyFromString(key, "EC"));
 		}
 		if (algo.equals(ES512.getAlgorithm())) {
-			return Algorithm.HMAC384(key); // EOT -----------------------------
+			return Algorithm.ECDSA512((ECKey) generatePublicKeyFromString(key, "EC"));
 		}
 		if (algo.equals(RS256.getAlgorithm())) {
 			return Algorithm.RSA256((RSAKey) generatePublicKeyFromString(key, "RSA"));
@@ -83,9 +99,10 @@ public class AlgorithmLinker {
 
 		return Algorithm.none();
 	}
-	
+
 	/**
-	 * @return gets the type (asymmetric, symmetric, none) of the provided @param algo
+	 * @return gets the type (asymmetric, symmetric, none) of the
+	 *         provided @param algo
 	 */
 	public static String getTypeOf(String algorithm) {
 		for (app.algorithm.AlgorithmWrapper supportedAlgorithm : supportedAlgorithms) {
@@ -95,7 +112,7 @@ public class AlgorithmLinker {
 		}
 		return AlgorithmType.none;
 	}
-	
+
 	public static app.algorithm.AlgorithmWrapper[] getSupportedAlgorithms() {
 		return supportedAlgorithms;
 	}
