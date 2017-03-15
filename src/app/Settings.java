a561a97b710906cb66938a049037705279ae010a
Oussama Zgheb <oz@oz-web.com>
2017-03-15 13:33:39 +0100

    Finalized setting variables

--- src/app/Settings.java
+++ src/app/Settings.java
@@ -8,17 +8,17 @@ public class Settings {
 	public static final String contextMenuString = "Send selected text to JWT4B Tab to decode";
 
 	public static final String tokenStateOriginal = "Original";
-	public static String tokenStateUpdated = "Token updated";
+	public static final String tokenStateUpdated = "Token updated";
 	
-	public static String verificationValid = "Signature verified";
-	public static String verificationInvalidKey = "Invalid key";
-	public static String verificationWrongKey = "Invalid Signature / wrong key";
+	public static final String verificationValid = "Signature verified";
+	public static final String verificationInvalidKey = "Invalid key";
+	public static final String verificationWrongKey = "Invalid Signature / wrong key";
 	
 	
 	public static final Boolean output = true;
 	
-	public static Color colorValid = Color.GREEN;
-	public static Color colorInvalid = Color.RED;
-	public static Color colorUndefined = Color.GRAY;
-	public static Color colorProblemInvalid = Color.YELLOW;
+	public static final Color colorValid = Color.GREEN;
+	public static final Color colorInvalid = Color.RED;
+	public static final Color colorUndefined = Color.GRAY;
+	public static final Color colorProblemInvalid = Color.YELLOW;
 }
