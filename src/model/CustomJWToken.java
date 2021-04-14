1669b27fa5596876dea2a8ec40f34c62d31fd26e
b4dpxl <b4d@pxl.me.uk>
2021-04-14 12:44:41 +0100

    Cleaned imports

--- src/model/CustomJWToken.java
+++ src/model/CustomJWToken.java
@@ -1,6 +1,8 @@
 package model;
 
-import java.io.*;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Date;
@@ -8,7 +10,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.TimeZone;
 import java.util.zip.GZIPInputStream;
-import java.util.zip.GZIPOutputStream;
 
 import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.codec.binary.StringUtils;
