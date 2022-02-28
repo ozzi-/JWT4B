c7073ae31dc6b155a8985b90fe08c34bab395e6e
DolphFlynn <96876199+DolphFlynn@users.noreply.github.com>
2022-02-28 13:31:52 +0000

    Move stdout and stderr PrintWriter instances to Output and make private. (#68)

--- src/app/helpers/Output.java
+++ src/app/helpers/Output.java
@@ -1,31 +1,35 @@
 package app.helpers;
 
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.TimeZone;
 
 public class Output {
+    private static final DateFormat DATE_FORMAT = new SimpleDateFormat("HH:mm:ss.SSS");
 
-  private static final SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss.SSS");
+    private static PrintWriter stdout = new PrintWriter(System.out, true);
+    private static PrintWriter stderr = new PrintWriter(System.err, true);
 
-  public static void output(String string) {
-    Date cal = Calendar.getInstance(TimeZone.getDefault()).getTime();
-    String msg = sdf.format(cal.getTime()) + " | " + string;
-    if (Config.stdout == null) {
-      System.out.println(msg);
-    } else {
-      Config.stdout.println(msg);
+    public static void initialise(OutputStream outputStream, OutputStream errorStream) {
+        stdout = new PrintWriter(outputStream, true);
+        stderr = new PrintWriter(errorStream, true);
     }
-  }
 
-  public static void outputError(String string) {
-    Date cal = Calendar.getInstance(TimeZone.getDefault()).getTime();
-    String msg = sdf.format(cal.getTime()) + " | " + string;
-    if (Config.stderr == null) {
-      System.err.println(msg);
-    } else {
-      Config.stderr.println(msg);
+    public static void output(String string) {
+        write(stdout, string);
     }
-  }
+
+    public static void outputError(String string) {
+        write(stderr, string);
+    }
+
+    private static void write(PrintWriter writer, String string) {
+        Date cal = Calendar.getInstance(TimeZone.getDefault()).getTime();
+        String msg = DATE_FORMAT.format(cal.getTime()) + " | " + string;
+        writer.println(msg);
+    }
 }
