88c624276bc3dd15dbf7116bda4dac761095a2a9
test <linzhihan@threathunter.cn>
2025-03-14 10:40:12 +0800

    自动收集JWT密钥并检验有效性

--- src/main/java/app/controllers/HighLightController.java
+++ src/main/java/app/controllers/HighLightController.java
@@ -6,16 +6,31 @@ import burp.api.montoya.http.handler.*;
 
 import app.helpers.Config;
 import app.tokenposition.ITokenPosition;
+import app.helpers.SecretFinder;
 
 // This controller handles the highlighting of entries in the HTTP history tab
 public class HighLightController implements HttpHandler {
 
+	private String validSecret;
+
 	@Override
 	public RequestToBeSentAction handleHttpRequestToBeSent(HttpRequestToBeSent requestToBeSent) {
-		boolean containsJWT = ITokenPosition.findTokenPositionImplementation(requestToBeSent, true) != null;
+		ITokenPosition tokenPosition = ITokenPosition.findTokenPositionImplementation(requestToBeSent, true);
+		boolean containsJWT = tokenPosition != null;
 		if (containsJWT) {
+
+			SecretFinder sfh = new SecretFinder(tokenPosition, requestToBeSent);
+			this.validSecret = sfh.getSecrets().stream()
+					.filter(sfh::checkSecret)
+					.findFirst()
+					.orElse(null);
+
 			updateAnnotations(requestToBeSent.annotations());
+		}else {
+			this.validSecret = null;
 		}
+
+
 
 		return RequestToBeSentAction.continueWith(requestToBeSent);
 	}
@@ -31,11 +46,17 @@ public class HighLightController implements HttpHandler {
 	}
 
 	private void updateAnnotations(Annotations annotations) {
+
 		if (!Config.interceptComment.isEmpty()) {
 			annotations.setNotes(Config.interceptComment);
 		}
 		if (!Config.highlightColor.equals("None")) {
 			annotations.setHighlightColor(HighlightColor.highlightColor(Config.highlightColor));
+		}
+
+		if(this.validSecret != null) {
+			annotations.setNotes(Config.SecretFoundInterceptComment.concat(this.validSecret));
+			annotations.setHighlightColor(HighlightColor.highlightColor(Config.SecretFoundHighlightColor));
 		}
 	}
 }
