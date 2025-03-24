daae17de8fddbd4194b358c12d0afa50c62e0e2e
ozzi- <oz@oz-web.com>
2025-03-24 11:47:46 +0100

    Update HighLightController.java
    small tweaks

--- src/main/java/app/controllers/HighLightController.java
+++ src/main/java/app/controllers/HighLightController.java
@@ -1,39 +1,40 @@
 package app.controllers;
 
+import app.helpers.Config;
+import app.helpers.SecretFinder;
+import app.tokenposition.ITokenPosition;
 import burp.api.montoya.core.Annotations;
 import burp.api.montoya.core.HighlightColor;
-import burp.api.montoya.http.handler.*;
-
-import app.helpers.Config;
-import app.tokenposition.ITokenPosition;
-import app.helpers.SecretFinder;
+import burp.api.montoya.http.handler.HttpHandler;
+import burp.api.montoya.http.handler.HttpRequestToBeSent;
+import burp.api.montoya.http.handler.HttpResponseReceived;
+import burp.api.montoya.http.handler.RequestToBeSentAction;
+import burp.api.montoya.http.handler.ResponseReceivedAction;
 
 // This controller handles the highlighting of entries in the HTTP history tab
 public class HighLightController implements HttpHandler {
 
 	private String validSecret;
-
+	
 	@Override
 	public RequestToBeSentAction handleHttpRequestToBeSent(HttpRequestToBeSent requestToBeSent) {
-		ITokenPosition tokenPosition = ITokenPosition.findTokenPositionImplementation(requestToBeSent, true);
-		boolean containsJWT = tokenPosition != null;
-		if (containsJWT) {
+			ITokenPosition tokenPosition = ITokenPosition.findTokenPositionImplementation(requestToBeSent, true);
+			boolean containsJWT = tokenPosition != null;
+			if (containsJWT) {
 
-			SecretFinder sfh = new SecretFinder(tokenPosition, requestToBeSent);
-			this.validSecret = sfh.collectSecrets().stream()
-					.filter(sfh::checkSecret)
-					.findFirst()
-					.orElse(null);
+				SecretFinder sfh = new SecretFinder(tokenPosition, requestToBeSent);
+				this.validSecret = sfh.collectSecrets().stream()
+						.filter(sfh::checkSecret)
+						.findFirst()
+						.orElse(null);
 
-			updateAnnotations(requestToBeSent.annotations());
-		}else {
-			this.validSecret = null;
+				updateAnnotations(requestToBeSent.annotations());
+			}else {
+				this.validSecret = null;
+			}
+
+			return RequestToBeSentAction.continueWith(requestToBeSent);
 		}
-
-
-
-		return RequestToBeSentAction.continueWith(requestToBeSent);
-	}
 
 	@Override
 	public ResponseReceivedAction handleHttpResponseReceived(HttpResponseReceived responseReceived) {
@@ -41,19 +42,17 @@ public class HighLightController implements HttpHandler {
 		if (containsJWT) {
 			updateAnnotations(responseReceived.annotations());
 		}
-
 		return ResponseReceivedAction.continueWith(responseReceived);
 	}
 
 	private void updateAnnotations(Annotations annotations) {
-
 		if (!Config.interceptComment.isEmpty()) {
 			annotations.setNotes(Config.interceptComment);
 		}
 		if (!Config.highlightColor.equals("None")) {
 			annotations.setHighlightColor(HighlightColor.highlightColor(Config.highlightColor));
 		}
-
+		
 		if(this.validSecret != null) {
 			annotations.setNotes(Config.SecretFoundInterceptComment.concat(this.validSecret));
 			annotations.setHighlightColor(HighlightColor.highlightColor(Config.SecretFoundHighlightColor));
